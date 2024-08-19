c7b8d2926e126949dfad1e3c094ba3b1d56b92c7
ranjithpn292 <ranjithpn292@gmail.com>
2024-08-19 22:35:29 +0530

    redux concept in an index file

---
+++ index.js
@@ -0,0 +1,57 @@
+const redux = require('redux')
+const createStore = redux.createStore
+
+const BUY_CAKE = 'BUY_CAKE'
+const BUY_ICECREAM = 'BUY_ICECREAM'
+
+
+// action in redux
+function buyCake(){
+  return {
+    type: BUY_CAKE,
+    info: 'first redux action'
+}
+}
+
+// action in redux
+function buyIceCream (){
+    return {
+      type: BUY_ICECREAM,
+  }
+  }
+
+const initialState = {
+    numOfCakes : 10,
+    numOfIceCreams : 20
+}
+
+const initialCakeState = {
+    numOfCakes : 10,
+}
+
+const initialIceCreamState = {
+    numOfIceCreams : 20
+}
+
+const reducer = (state = initialState, action) => {
+   switch(action.type){
+    case BUY_CAKE: return {
+        ...state,
+        numOfCakes : state.numOfCakes - 1
+    }
+    case BUY_ICECREAM: return {
+        ...state,
+        numOfIceCreams : state.numOfIceCreams - 1
+    }
+
+    default : return state
+   }
+}
+
+const store = createStore(reducer)
+console.log('Initial state',store.getState())
+const unsubscribe = store.subscribe(() => console.log('Updated State',store.getState()))
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+store.dispatch(buyIceCream())
+unsubscribe()
