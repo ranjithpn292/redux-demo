6986f1986c6d0e65c46e3545da2f029ed68098bb
ranjithpn292 <ranjithpn292@gmail.com>
2024-08-19 22:46:55 +0530

    appplying logger middleware to a Reducer

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const redux = require('redux')
+const reduxLogger = require('redux-logger')
+
 const createStore = redux.createStore
 const combineReducers = redux.combineReducers
+const logger = reduxLogger.createLogger()
+const applyMiddleware = redux.applyMiddleware
 
 const BUY_CAKE = 'BUY_CAKE'
 const BUY_ICECREAM = 'BUY_ICECREAM'
@@ -73,9 +77,9 @@ const cakeReducer = (state = initialCakeState, action) => {
     iceCream : iceCreamReducer
  })
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer,applyMiddleware(logger))
 console.log('Initial state',store.getState(),store.getState())
-const unsubscribe = store.subscribe(() => console.log('Updated State',store.getState(),store.getState()))
+const unsubscribe = store.subscribe(() => {})
 store.dispatch(buyCake())
 store.dispatch(buyCake())
 store.dispatch(buyIceCream())
